e3adb1d55867820c144ea1f8eb8fe1db4b2f902f
erik suserud <eriksuserud@gmail.com>
2019-09-19 15:27:17 +0200

    ovning1-klar

---
+++ Ovning1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ovning1
+{
+    class Program
+    {
+        /*
+        Skapa en metod Hello() som tar en string variabel som indata och skriver
+        ut texten med ett annat meddelande 
+         */
+        static void Main(string[] args)
+        {
+            Hello("Hej");
+        }
+        static void Hello(string input)
+        {
+            System.Console.WriteLine(input);
+        }
+    }
+}
