d5ec1add3f9b9061210514db06dbd47ae65ee4e3
erik suserud <eriksuserud@gmail.com>
2019-09-23 14:36:41 +0200

    Övning5-klar

---
+++ Övning5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Övning5
+{
+    class Program
+    {
+        /*
+        Fortsättning på föregående övning. Gör om funktionen så att man i
+        metodanropet (via parameterlistan) får ange vad momsen ligger på. 
+         */
+        static void Main(string[] args)
+        {
+           System.Console.Write("Vad kostar produkt: ");
+           double value = Convert.ToDouble(Console.ReadLine());
+           System.Console.Write("Ange moms, 0,01->0,99(1%-99%): ");
+           double tax = Convert.ToDouble(Console.ReadLine());
+           double totalTax = TaxCalcu(value,tax);
+           System.Console.WriteLine("Moms: {0}",totalTax);
+           double toltalPriceWithTax = value + totalTax;
+           System.Console.WriteLine("Totalpris ink. moms: {0}", toltalPriceWithTax);
+
+            
+        }
+        static double TaxCalcu(double productValue, double taxValue)
+        {
+            double totalValue = productValue * taxValue;
+            return totalValue;
+
+        }
+    }   
+
+}
