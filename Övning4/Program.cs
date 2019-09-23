55704f5be0bcab1ddf9b2889dde717808bd3da46
erik suserud <eriksuserud@gmail.com>
2019-09-23 11:29:02 +0200

    övning4-klar

---
+++ Övning4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Övning4
+{
+    class Program
+    {
+        /*
+        Skapa en metod som lägger till momsen på en viss summa. Antag att
+        momsen är 25%. 
+         */
+        static void Main(string[] args)
+        {
+            System.Console.Write("Hur mycket kostnar produkten(i SEK): ");
+            double productVal = Convert.ToDouble(Console.ReadLine());
+            double productValueWithTax = Tax(productVal);
+            System.Console.WriteLine("Kostnaden för produkt ink. 25% moms är: {0}", productValueWithTax);
+            productValueWithTax = Math.Round(productValueWithTax,2);
+            System.Console.WriteLine("Avrundad decimal = {0}",productValueWithTax);
+        }
+        static double Tax (double tax)
+        {
+            return tax * 1.25;
+        }
+    }
+}
