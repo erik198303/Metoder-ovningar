c5a094e84a553c402e8df50029096bfc436ef3b4
erik suserud <eriksuserud@gmail.com>
2019-09-24 13:05:34 +0200

    Övning7-klar

---
+++ Övning7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Övning7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ifMyndig = Myndig(17);
+            System.Console.WriteLine("Du är myndig = {0}", ifMyndig);
+        }
+      static bool Myndig(int age)
+     {
+            if (age >= 18)
+         {
+            return true;
+            //System.Console.WriteLine("Grattis! Spring och köpa brews!");
+         }
+          else
+         {   return false;
+            //System.Console.WriteLine("Tyvärr, då får dricka Fanta!");
+         }
+     }
+
+
+    }
+}
